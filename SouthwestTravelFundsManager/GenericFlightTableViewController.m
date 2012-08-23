69f086fd1c8de1c7cfe0b8021f63a641e28993f9
Colin Regan <colin@redcup.la>
2012-08-23 04:02:45 -0700

    Added field validation code

--- SouthwestTravelFundsManager/GenericFlightTableViewController.m
+++ SouthwestTravelFundsManager/GenericFlightTableViewController.m
@@ -95,14 +95,62 @@
 
 - (void)setCustomInputViews {
     self.flightTextField.inputView = self.airportPicker;
-}
-
-- (void)textFieldDidEndEditing:(UITextField *)textField {
-    [textField resignFirstResponder];
+    self.costTextField.keyboardType = UIKeyboardTypeDecimalPad;
+    
+    // set up date pickers
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    // move first responder to next text field
+    if ([textField isEqual:self.confirmTextField]) [self.costTextField becomeFirstResponder];
     return TRUE;
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    NSString *newString = [textField.text stringByReplacingCharactersInRange:range withString:string];
+    if ([textField isEqual:self.confirmTextField]) {
+        // move first responder after 6 characters in confirmation code
+        if (newString.length == 6) {
+            self.confirmTextField.text = newString;
+            [self.costTextField becomeFirstResponder];
+            return FALSE;
+        }
+    } else if ([textField isEqual:self.costTextField]) {
+        // disallow deletion of "$" character
+        if (range.location == 0) return FALSE;
+        // disallow multiple decimal points
+        if ([string isEqualToString:@"."] && [textField.text rangeOfString:@"."].length) return FALSE;
+        // move first responder with two digits after decimal point
+        if (newString.length >= 3 && [newString characterAtIndex:newString.length - 3] == '.') {
+            self.costTextField.text = newString;
+            [self.expirationTextField becomeFirstResponder];
+            return FALSE;
+        }
+    }
+    return TRUE;
+}
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    if ([textField isEqual:self.costTextField]) {
+        // add currency symbol when empty
+        if (!self.costTextField.text.length) self.costTextField.text = @"$";
+    }
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    // update corresponding property
+    if ([textField isEqual:self.confirmTextField]) {
+        self.confirmationCode = self.confirmTextField.text;
+        NSLog(@"%@", self.confirmationCode);
+    } else if ([textField isEqual:self.costTextField]) {
+        NSString *numberValue;
+        if ([self.costTextField.text hasPrefix:@"$"]) numberValue = [self.costTextField.text substringFromIndex:1];
+        self.cost = [NSNumber numberWithDouble:[numberValue doubleValue]];
+        // TODO: use NSNumberFormatter to update text display
+        NSLog(@"%@", self.cost);
+    } else if ([textField isEqual:self.expirationTextField]) {
+        
+    }
 }
 
 - (void)airportPickerViewController:(AirportPickerViewController *)airportPickerVC selectedOrigin:(NSString *)origin andDestination:(NSString *)destination {
