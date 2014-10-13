75408573a5be1bd8274e569313318281f9ea5cc6
Colin Regan <colin@redcup.la>
2014-10-13 14:42:16 -0400

    Adds TravelFund model

---
+++ Swift/Southwest Travel Manager/Southwest Travel Manager/TravelFund.swift
@@ -0,0 +1,17 @@
+//
+//  TravelFund.swift
+//  Southwest Travel Manager
+//
+//  Created by Colin Regan on 10/13/14.
+//  Copyright (c) 2014 Red Cup. All rights reserved.
+//
+
+import UIKit
+import Realm
+
+class TravelFund: RLMObject {
+    dynamic var balance = 0.0
+    dynamic var expirationDate = NSDate(timeIntervalSinceNow: 60 * 60 * 24 * 365)
+    dynamic var notes = ""
+    dynamic var unusedTicket = true
+}
