61d17b85df9aa8a4646f0a412863e7d7ee6cc4c9
Colin Regan <colin@redcup.la>
2014-10-13 18:00:27 -0400

    Adds prototype cell in storyboard

--- Swift/Southwest Travel Manager/Southwest Travel Manager/NewFlightVC.swift
+++ Swift/Southwest Travel Manager/Southwest Travel Manager/NewFlightVC.swift
@@ -26,7 +26,7 @@ class NewFlightVC: UITableViewController {
     }
 
     // MARK: - Table view data source
-
+    /*
     override func numberOfSectionsInTableView(tableView: UITableView!) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
@@ -38,7 +38,7 @@ class NewFlightVC: UITableViewController {
         // Return the number of rows in the section.
         return 0
     }
-
+    */
     /*
     override func tableView(tableView: UITableView!, cellForRowAtIndexPath indexPath: NSIndexPath!) -> UITableViewCell! {
         let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
